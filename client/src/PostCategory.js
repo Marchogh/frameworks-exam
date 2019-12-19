6cb0e3418f58d88de5ad8ba0d3577397459caf06
Marc Høgh <Marchogh@hotmail.com>
2019-12-19 21:23:35 +0100

    Fixed postcat

--- client/src/PostCategory.js
+++ client/src/PostCategory.js
@@ -57,7 +57,7 @@ export default class PostCategory extends Component {
                     </button>
                 </div>
             </form>
-            <p>Delete knappen virker ikke i frontend delen - Det virker kun i backend når man laver en POST request til api/books//delte-category</p>
+            <p>Delete knappen virker ikke i frontend delen - Det virker kun i backend når man laver en POST request til api/books/delte-category</p>
                 {categories}
                 </div>
             </section>
